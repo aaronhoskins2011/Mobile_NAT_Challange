2dd9bcd4b99fa4c7150a372cdb52a437f0280c4c
aaronhoskins2011 <aaronhoskins2011@gmail.com]>
2017-09-06 13:51:49 -0400

    Added some analytics

--- app/src/main/java/com/example/aaronhoskins/mobile_nat_challange/view/main_activity/MainActivity.java
+++ app/src/main/java/com/example/aaronhoskins/mobile_nat_challange/view/main_activity/MainActivity.java
@@ -2,21 +2,22 @@ package com.example.aaronhoskins.mobile_nat_challange.view.main_activity;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.ContentViewEvent;
 import com.example.aaronhoskins.mobile_nat_challange.R;
 import com.example.aaronhoskins.mobile_nat_challange.data.remote.WeatherDataHandler;
 import com.example.aaronhoskins.mobile_nat_challange.model.ThreadObjectReturns;
-import com.example.aaronhoskins.mobile_nat_challange.model.conditions.WeatherProfile;
 import com.example.aaronhoskins.mobile_nat_challange.model.forecast.ForecastProfile;
 import com.example.aaronhoskins.mobile_nat_challange.view.CurrentConditionsFragment.CurrentConditionsFragment;
 import com.example.aaronhoskins.mobile_nat_challange.view.ForecastFragment.ForecastFragment;
 
-import java.io.IOException;
+import io.fabric.sdk.android.Fabric;
 
 import static com.example.aaronhoskins.mobile_nat_challange.data.local.Constants.CURRENT_CONDITIONS_FRAG_ID;
 import static com.example.aaronhoskins.mobile_nat_challange.data.local.Constants.FORECAST_FRAG_ID;
@@ -34,12 +35,21 @@ public class MainActivity extends AppCompatActivity implements CurrentConditions
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Answers());
         setContentView(R.layout.activity_main);
         etZipCode = (EditText)findViewById(R.id.etZipCode);
         wdh = new WeatherDataHandler();
        if(!(getIntent().getStringExtra(PASS_ZIP_CODE_STRING) == null)){
            zipCode = getIntent().getStringExtra(PASS_ZIP_CODE_STRING);
        }
+        // TODO: Use your own attributes to track content views in your app
+        Answers.getInstance().logContentView(new ContentViewEvent()
+                .putContentName("Zip_Code")
+                .putContentType("String")
+                .putContentId(zipCode)
+                .putCustomAttribute("Favorites Count", 20)
+                .putCustomAttribute("Screen Orientation", "Landscape"));
+
         info = mainActivityPresenter.initInformation(zipCode);
         initFragmentsForActivity();
     }
